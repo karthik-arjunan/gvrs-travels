cfaa9f3538a6f6a0701fc589a0f96e20b4187646
karthikeyan.arjunan <KArjunan@eab.com>
2026-01-19 21:42:05 +0530

    basic backend

--- gvrs-travels/api/travel/models.py
+++ gvrs-travels/api/travel/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class BookingCounter(models.Model):
+    year = models.IntegerField(unique=True)
+    counter = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.year} - {self.counter}"
--- gvrs-travels/api/travel/views.py
+++ gvrs-travels/api/travel/views.py
@@ -2,9 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import LoginSerializer
-from .services.whatsapp_service import send_whatsapp_message
 from datetime import datetime
-
+from .utils.booking_id import generate_booking_id
 @api_view(['POST'])
 def login_view(request):
     serializer = LoginSerializer(data=request.data)
@@ -15,49 +14,12 @@ def login_view(request):
         )
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def generate_booking_id():
-    year = datetime.now().year
-    timestamp = int(datetime.now().timestamp())
-    return f"GVRS-{year}-{timestamp}"
 
 @api_view(["POST"])
 def create_booking(request):
     data = request.data
 
     booking_id = generate_booking_id()
-
-    customer_message = f"""
-✅ Booking Confirmed
-Booking ID: {booking_id}
-
-Customer: {data['customerName']}
-Pickup: {data['pickup']}
-Drop: {data['drop']}
-
-Driver:
-{data['driverName']}
-{data['driverPhone']}
-
-GVRS Travels
-"""
-
-    driver_message = f"""
-🚘 New Trip Assigned
-
-Booking ID: {booking_id}
-
-Customer:
-{data['customerName']}
-{data['customerPhone']}
-
-Pickup: {data['pickup']}
-Drop: {data['drop']}
-
-GVRS Travels
-"""
-
-    send_whatsapp_message(data["customerPhone"], customer_message)
-    send_whatsapp_message(data["driverPhone"], driver_message)
 
     return Response({
         "success": True,
