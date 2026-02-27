aed6ec8107b9018679c0647d81c76a51bef36ae2
karthikeyan.arjunan <KArjunan@eab.com>
2026-02-27 20:15:58 +0530

    driver reposts

--- gvrs-travels/web/src/components/Header.jsx
+++ gvrs-travels/web/src/components/Header.jsx
@@ -1,7 +1,8 @@
+import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
 import "../pages/dashboard/dashboard.css";
-
+import { FiLogOut, FiChevronDown } from "react-icons/fi";
 const Header = () => {
   const navigate = useNavigate();
 
@@ -10,13 +11,56 @@ const Header = () => {
     sessionStorage.clear();
     navigate("/login");
   };
+  const [openMenu, setOpenMenu] = useState(false);
+  const dropdownRef = useRef(null);
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpenMenu(true);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const userName = "Raja Gunasekaran";
+  const userRole = "Admin";
   return (
     <header className="app-header">
-      <img src={logo} alt="GVRS Travels" className="app-logo" />
-      <button className="logout-btn" onClick={handleLogout}>
-        Logout
-      </button>
+      <div className="header-left">
+        <img src={logo} alt="GVRS Travels" className="app-logo" />
+      </div>
+
+      <div className="header-right" ref={dropdownRef}>
+        <div
+          className="premium-user-card"
+          onClick={() => setOpenMenu(!openMenu)}
+        >
+          <div className="premium-avatar">
+            {userName?.charAt(0).toUpperCase()}
+          </div>
+
+          <div className="premium-user-info">
+            <div className="premium-user-name">{userName}</div>
+            <div className="premium-user-role">{userRole}</div>
+          </div>
+
+          <FiChevronDown
+            className={`dropdown-arrow ${openMenu ? "rotate" : ""}`}
+          />
+        </div>
+
+        {openMenu && (
+          <div className="header-dropdown">
+            <div className="dropdown-item logout" onClick={handleLogout}>
+              <FiLogOut className="logout-icon" />
+              Logout
+            </div>
+          </div>
+        )}
+      </div>
     </header>
   );
 };
